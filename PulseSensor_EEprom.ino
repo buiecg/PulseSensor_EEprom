15e612fc73ffa0844a6b980b9afecbd29e7caa62
Claudio <16813492+buiecg@users.noreply.github.com>
2020-01-17 17:27:18 +0200

    Update PulseSensor_EEprom.ino

--- PulseSensor_EEprom.ino
+++ PulseSensor_EEprom.ino
@@ -29,7 +29,7 @@
    See ProcessEverySample.ino for an example of not using interrupts.
 */
 #define USE_ARDUINO_INTERRUPTS true
-#include <PulseSensorPlayground.h>
+#include <PulseSensorPlayground.h>  //Tools/ Manage Libraries... add PulseSensorPlayground
 #include <EEPROM.h>
 #include <avr/sleep.h>
 /*
@@ -58,9 +58,10 @@ const int OUTPUT_TYPE = PROCESSING_VISUALIZER;
 const int PULSE_INPUT = A0;
 const int PULSE_BLINK = 13;    // Pin 13 is the on-board LED
 const int PULSE_FADE = 5;
-const int THRESHOLD = 550;   // Adjust this number to avoid noise when idle
-int stay,  //for delay 31 BPM
-    adr_EPR;//EEPROM address
+const int ON_OFF_PULSE = 6;
+const int THRESHOLD = 650;   // Adjust this number to avoid noise when idle
+int stay,  //for delay BPM
+    adr_EPR = 0;//EEPROM address
 /*
    All the PulseSensor Playground functions.
 */
@@ -76,7 +77,7 @@ PulseSensorPlayground pulseSensor;
         [link]
 */
 
-const int  BUTTON_PIN = 3;     // EEPROM condition button
+const int  BUTTON_PIN = 3;     // EEPROM condition button + intrerupt state
 bool buttonState = 0;         // variable for reading the switch button status
 const int PIN_SPEAKER = 2;    // speaker on pin2 makes a beep with heartbeat
 
@@ -91,29 +92,44 @@ void setup() {
      of readSensor() calls, which would make the pulse measurement
      not work properly.
   */
-  pinMode(BUTTON_PIN, INPUT_PULLUP);
+  pinMode(BUTTON_PIN, INPUT_PULLUP); // check for LOW = EEPROM read
+  pinMode(4, INPUT_PULLUP);//useless setting, conected with BUTTON_PIN for easy fit DIP switch on the board NANO
+  pinMode(ON_OFF_PULSE, OUTPUT);
+  digitalWrite(ON_OFF_PULSE, LOW);
    Serial.begin(115200);
    buttonState = digitalRead(BUTTON_PIN);
    if (buttonState == LOW) {
-      attachInterrupt(digitalPinToInterrupt(3), wakeUpNow, RISING); // use interrupt 1 (pin 3) and run function
+      attachInterrupt(digitalPinToInterrupt(BUTTON_PIN), wakeUpNow, RISING); // use interrupt 1 (pin 3) and run function
                                       // wakeUpNow(function at the end of sketch) when pin 3 gets HIGH 
       while (!Serial) {
       ; // wait for serial port to connect. Needed for native USB port only
       }
-      Serial.println(F("CLEARDATA")); //for PLX-DAQ (EXCEL file)
-      Serial.println(F("LABEL, Data, Curent_time, Curent_number, BPM"));
+      Serial.println(F("CLEARDATA")); //for PLX-DAQ2 (EXCEL file)
+      Serial.println(F("LABEL, Data, Curent_number, BPM"));
       for ( int index = 0 ; index < EEPROM.length(); index++) { // send data from EEPROM to Excel file
         byte val_EPR = EEPROM.read(index);
-        Serial.println( (String) "DATA,DATE,TIME, " + index + "," + val_EPR + ",AUTOSCROLL_20" );
-        delay(100);
+        delay(80);//test between 80 and 100 for optimal transfer to excel PLX-DAQ2
+        if (val_EPR == 255){//find the marker and stop data transfer 
+          goto Final;
+        }
+        Serial.println( (String) "DATA,DATE, " + index + "," + val_EPR + ",AUTOSCROLL_20" );
+        
       }
+ Final: 
+      Serial.println(F("BEEP"));
+      Serial.println(F("DONE"));//Will flush the Serial port on Excel side PLX-DAQ2
+      Serial.end();
    set_sleep_mode(SLEEP_MODE_PWR_DOWN);   // sleep mode set 
    sleep_enable();
    sleep_cpu();
-    } else {
-    }
+    } 
   // Configure the PulseSensor manager.
-
+  
+  digitalWrite(ON_OFF_PULSE, HIGH);
+    Serial.begin(115200);
+    while (!Serial) {
+      ; // wait for serial port to connect. Needed for native USB port only
+     }
   pulseSensor.analogInput(PULSE_INPUT);
 //  pulseSensor.blinkOnPulse(PULSE_BLINK);
   pulseSensor.fadeOnPulse(PULSE_FADE);
@@ -161,16 +177,17 @@ void loop() {
    */
   if (pulseSensor.sawStartOfBeat()) {
     pulseSensor.outputBeat();
-    if ((buttonState == LOW) && (adr_EPR < EEPROM.length())){//
+    tone(PIN_SPEAKER,1500);              // tone(pin,frequency)
+    if ((buttonState == LOW) && (adr_EPR < EEPROM.length())){//check for EEPROM write
       stay += 1;
     }
      if (stay >= 15){     // save in EEPROM ones in 16th BPM reads
         stay = 0;          //aprox 3 hours BPM in 1024 memories
         EEPROM.write(adr_EPR, (pulseSensor.getBeatsPerMinute()));
         adr_EPR += 1;
-        digitalWrite(PULSE_BLINK, bitRead(adr_EPR, 0));
+        digitalWrite(PULSE_BLINK, bitRead(adr_EPR, 0));//pin pt LED 16 BPM OFF, 16 BPM ON, for EEPROM check
+        EEPROM.write(adr_EPR,255);//stop marker - for partial new logs over previous logs
       }
-    tone(PIN_SPEAKER,1500);              // tone(pin,frequency)
   }
 
   /*
@@ -184,6 +201,10 @@ void loop() {
 }
 void wakeUpNow()        // here the interrupt is handled after wakeup
 {
+      Serial.begin(115200);
+    while (!Serial) {
+      ; // wait for serial port to connect. Needed for native USB port only
+     }
   // execute code here after wake-up before returning to the loop() function
   // timers and code using timers (serial.print and more...) will not work here.
   // we don't really need to execute any special functions here, since we
